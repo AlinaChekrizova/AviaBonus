a7c112faa9e9a345b9be3bacb0d5f6579ef80f5e
Alina Chekrizova <alinaspleen@gmail.com>
2021-09-16 18:11:48 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15347_30;
+        int mile = 20;
+
+        int bonus = ticketPrice / mile / 100;
+        System.out.println(bonus);
+    }
+}
